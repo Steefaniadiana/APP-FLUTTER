242b79a719fd489acdb0845b2f0d016b553d8e28
Stefania Diana <steefaniadiana@gmail.com>
2024-04-09 17:45:11 +0200

    app

---
+++ lib/screens/revelare_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RevelareScreen extends StatefulWidget {
+  const RevelareScreen({super.key});
+
+  @override
+  State<RevelareScreen> createState() => _RevelareScreenState();
+}
+
+class _RevelareScreenState extends State<RevelareScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('RevelareScreen'),
+      ),
+    );
+  }
+}
